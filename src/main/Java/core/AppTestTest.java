4c52ee7e3d06518daf2ac584e57618bbd6c050ab
balatskiyart <balatskiyart@outlook.com>
2016-03-22 19:53:57 -0700

    Homework 38

---
+++ src/main/Java/core/AppTestTest.java
@@ -0,0 +1,27 @@
+package core;
+
+/**
+ * Created by Artem on 3/21/16.
+ */
+public class AppTestTest {
+
+    @org.junit.Test
+    public void testBeforeClass() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAfterClass() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testBefore() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAfter() throws Exception {
+
+    }
+}
